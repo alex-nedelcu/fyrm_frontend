86e3269ca0c3026816f25e77503cf892ea203ac9
alex-nedelcu <alexandru.nedelcu01@yahoo.com>
2022-12-12 15:19:16 +0200

    Test pre-commit hook

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    print("abcd");
-
     return MaterialApp(
       title: 'Flutter   Demo',
       theme: ThemeData(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:fyrm_frontend/main.dart';
 
 void main() {
