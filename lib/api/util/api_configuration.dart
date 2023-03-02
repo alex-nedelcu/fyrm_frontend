ac46e9b3d4fee7c1c2931eff23ff48cd801d6cda
alex-nedelcu <alexandru.nedelcu01@yahoo.com>
2023-03-02 19:25:34 +0200

    Progress

--- lib/api/util/api_configuration.dart
+++ lib/api/util/api_configuration.dart
@@ -1,7 +1,7 @@
 class ApiConfiguration {
   static const String localBaseUrl = "http://localhost:8080/api/v1/fyrm";
-  static const String externalBaseUrl = "http://192.168.1.128:8080/api/v1/fyrm";
-  static const String baseUrl = localBaseUrl;
+  static const String externalBaseUrl = "http://192.168.1.129:8080/api/v1/fyrm";
+  static const String baseUrl = externalBaseUrl;
   static Map<String, String> writeOperationHeaders = <String, String>{
     'Content-Type': 'application/json; charset=UTF-8',
     'Accept': '*/*'
