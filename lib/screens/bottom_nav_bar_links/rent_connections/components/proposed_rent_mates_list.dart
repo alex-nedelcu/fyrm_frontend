245fb46ad9cd21fae640588d7b4c6d21c3a04c79
alex-nedelcu <alexandru.nedelcu01@yahoo.com>
2023-03-14 19:36:27 +0200

    Progress

--- lib/screens/bottom_nav_bar_links/rent_connections/components/proposed_rent_mates_list.dart
+++ lib/screens/bottom_nav_bar_links/rent_connections/components/proposed_rent_mates_list.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fyrm_frontend/api/rent_connections/dto/proposed_rent_mate_dto.dart';
+import 'package:fyrm_frontend/helper/size_configuration.dart';
 import 'package:fyrm_frontend/screens/bottom_nav_bar_links/rent_connections/components/rent_mate_card.dart';
 
 class ProposedRentMatesList extends StatefulWidget {
@@ -16,12 +16,29 @@ class _ProposedRentMatesListState extends State<ProposedRentMatesList> {
   @override
   Widget build(BuildContext context) {
     final rentMates = widget.rentMates;
-    return SingleChildScrollView(
-      child: ListView(
-        physics: const BouncingScrollPhysics(),
-        shrinkWrap: true,
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        children: rentMates.map((rentMate) => RentMateCard(rentMate: rentMate)).toList(),
+    return rentMates.isEmpty
+        ? buildEmptyProposalMessage()
+        : SingleChildScrollView(
+            child: ListView(
+              physics: const BouncingScrollPhysics(),
+              shrinkWrap: true,
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              children: rentMates.map((rentMate) => RentMateCard(rentMate: rentMate)).toList(),
+            ),
+          );
+  }
+
+  Widget buildEmptyProposalMessage() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 60, right: 25, left: 25),
+      child: Text(
+        "Unfortunately there is no matching rent mate for you at the moment...",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.black26,
+          fontSize: getProportionateScreenWidth(17),
+          fontWeight: FontWeight.bold,
+        ),
       ),
     );
   }
