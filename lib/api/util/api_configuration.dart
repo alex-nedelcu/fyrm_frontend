63bdf353c9c5f71e2528e7424eba7c68522ca50f
alex-nedelcu <alexandru.nedelcu01@yahoo.com>
2023-06-30 11:34:04 +0300

    update ip

--- lib/api/util/api_configuration.dart
+++ lib/api/util/api_configuration.dart
@@ -1,6 +1,6 @@
 class ApiConfiguration {
   static const String localBaseUrl = "http://localhost:8080/api/v1/fyrm";
-  static const String externalBaseUrl = "http://192.168.1.132:8080/api/v1/fyrm";
+  static const String externalBaseUrl = "http://172.20.10.11:8080/api/v1/fyrm";
   static const String baseUrl = externalBaseUrl;
   static Map<String, String> writeOperationHeaders = <String, String>{
     'Content-Type': 'application/json; charset=UTF-8',
